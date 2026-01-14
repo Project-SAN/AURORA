b94d637010d38e6fdd39d5368519d14fe5e1ab92
quantumshiro <hiro.1nakanishi@gmail.com>
2026-01-14 12:38:44 +0900

    add: kernal heaps

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,6 +1,10 @@
 #![no_std]
 #![no_main]
+#![feature(alloc_error_handler)]
 
+extern crate alloc;
+
+mod heap;
 mod serial;
 
 use core::panic::PanicInfo;
@@ -10,6 +14,7 @@ use uefi::table::boot::MemoryType;
 #[entry]
 fn main(_handle: Handle, system_table: SystemTable<Boot>) -> Status {
     serial::init();
+    heap::init();
     serial::write(format_args!("Hello from AURORA UEFI kernel\n"));
 
     let (_rt, memory_map) = system_table.exit_boot_services(MemoryType::LOADER_DATA);
@@ -27,6 +32,14 @@ fn main(_handle: Handle, system_table: SystemTable<Boot>) -> Status {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     serial::write(format_args!("PANIC: {}\n", info));
+    loop {
+        unsafe { core::arch::asm!("hlt"); }
+    }
+}
+
+#[alloc_error_handler]
+fn alloc_error(layout: core::alloc::Layout) -> ! {
+    serial::write(format_args!("OOM: {:?}\n", layout));
     loop {
         unsafe { core::arch::asm!("hlt"); }
     }
